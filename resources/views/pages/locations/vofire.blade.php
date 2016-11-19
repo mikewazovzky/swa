f1167716e549c279ae648612257764b4283a5d43
Mike Wazovzky <mike.wazovzky@gmail.com>
2016-11-19 18:10:43 +0300

    vofire location pages

--- resources/views/pages/locations/vofire.blade.php
+++ resources/views/pages/locations/vofire.blade.php
@@ -2,7 +2,7 @@
 
 @section('location')
 
-<img src="media/vofire.jpg" align="left" width="300px" style="margin: 0 10px 0 0;"/>
+<img src="/media/vofire.jpg" align="left" width="300px" style="margin-right: 10px;"/>
 
 <p>	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 	Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
@@ -21,152 +21,88 @@
 </p>
 
 <hr>
+<div class="small">
 
-<img class="small_200" src="{{imgs.201.src}}" title="{{imgs.201.title}}" alt="{{imgs.201.alt}}" />
-<img class="small_200" src="{{imgs.202.src}}" title="{{imgs.202.title}}" alt="{{imgs.202.alt}}" />
-<img class="small_200" src="{{imgs.203.src}}" title="{{imgs.203.title}}" alt="{{imgs.203.alt}}" />
-<img class="small_200" src="{{imgs.204.src}}" title="{{imgs.204.title}}" alt="{{imgs.204.alt}}" />
-<img class="small_200" src="{{imgs.205.src}}" title="{{imgs.205.title}}" alt="{{imgs.205.alt}}" />
-<img class="small_200" src="{{imgs.206.src}}" title="{{imgs.206.title}}" alt="{{imgs.206.alt}}" />
-<img class="small_200" src="{{imgs.207.src}}" title="{{imgs.207.title}}" alt="{{imgs.207.alt}}" />
-<img class="small_200" src="{{imgs.208.src}}" title="{{imgs.208.title}}" alt="{{imgs.208.alt}}" />
-<img class="small_200" src="{{imgs.209.src}}" title="{{imgs.209.title}}" alt="{{imgs.209.alt}}" />
-<img class="small_200" src="{{imgs.210.src}}" title="{{imgs.210.title}}" alt="{{imgs.210.alt}}" />
-
-<hr>
-
-<img class="small_200" src="{{imgs.211.src}}" title="{{imgs.211.title}}" alt="{{imgs.211.alt}}" />
-<img class="small_200" src="{{imgs.212.src}}" title="{{imgs.212.title}}" alt="{{imgs.212.alt}}" />
-<img class="small_200" src="{{imgs.213.src}}" title="{{imgs.213.title}}" alt="{{imgs.213.alt}}" />
-<img class="small_200" src="{{imgs.214.src}}" title="{{imgs.214.title}}" alt="{{imgs.214.alt}}" />
-<img class="small_200" src="{{imgs.215.src}}" title="{{imgs.215.title}}" alt="{{imgs.215.alt}}" />
-<img class="small_200" src="{{imgs.216.src}}" title="{{imgs.216.title}}" alt="{{imgs.216.alt}}" />
-<img class="small_200" src="{{imgs.217.src}}" title="{{imgs.217.title}}" alt="{{imgs.217.alt}}" />
-<img class="small_200" src="{{imgs.218.src}}" title="{{imgs.218.title}}" alt="{{imgs.218.alt}}" />
-<img class="small_200" src="{{imgs.219.src}}" title="{{imgs.219.title}}" alt="{{imgs.219.alt}}" />
-<img class="small_200" src="{{imgs.220.src}}" title="{{imgs.220.title}}" alt="{{imgs.220.alt}}" />
-
-<hr>
-
-<img class="small_200" src="{{imgs.221.src}}" title="{{imgs.221.title}}" alt="{{imgs.221.alt}}" />
-<img class="small_200" src="{{imgs.222.src}}" title="{{imgs.222.title}}" alt="{{imgs.222.alt}}" />
-<img class="small_200" src="{{imgs.223.src}}" title="{{imgs.223.title}}" alt="{{imgs.223.alt}}" />
-<img class="small_200" src="{{imgs.224.src}}" title="{{imgs.224.title}}" alt="{{imgs.224.alt}}" />
-<img class="small_200" src="{{imgs.225.src}}" title="{{imgs.225.title}}" alt="{{imgs.225.alt}}" />
-<img class="small_200" src="{{imgs.226.src}}" title="{{imgs.226.title}}" alt="{{imgs.226.alt}}" />
-<img class="small_200" src="{{imgs.227.src}}" title="{{imgs.227.title}}" alt="{{imgs.227.alt}}" />
-<img class="small_200" src="{{imgs.228.src}}" title="{{imgs.228.title}}" alt="{{imgs.228.alt}}" />
-<img class="small_200" src="{{imgs.229.src}}" title="{{imgs.229.title}}" alt="{{imgs.229.alt}}" />
-<img class="small_200" src="{{imgs.230.src}}" title="{{imgs.230.title}}" alt="{{imgs.230.alt}}" />
-
-<hr>
-
-<img class="small_200" src="{{imgs.231.src}}" title="{{imgs.231.title}}" alt="{{imgs.231.alt}}" />
-<img class="small_200" src="{{imgs.232.src}}" title="{{imgs.232.title}}" alt="{{imgs.232.alt}}" />
-<img class="small_200" src="{{imgs.233.src}}" title="{{imgs.233.title}}" alt="{{imgs.233.alt}}" />
-<img class="small_200" src="{{imgs.234.src}}" title="{{imgs.234.title}}" alt="{{imgs.234.alt}}" />
-<img class="small_200" src="{{imgs.235.src}}" title="{{imgs.235.title}}" alt="{{imgs.235.alt}}" />
-<img class="small_200" src="{{imgs.236.src}}" title="{{imgs.236.title}}" alt="{{imgs.236.alt}}" />
-<img class="small_200" src="{{imgs.237.src}}" title="{{imgs.237.title}}" alt="{{imgs.237.alt}}" />
-<img class="small_200" src="{{imgs.238.src}}" title="{{imgs.238.title}}" alt="{{imgs.238.alt}}" />
-<img class="small_200" src="{{imgs.239.src}}" title="{{imgs.239.title}}" alt="{{imgs.239.alt}}" />
-<img class="small_200" src="{{imgs.240.src}}" title="{{imgs.240.title}}" alt="{{imgs.240.alt}}" />
-
-<hr>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<img class="small_200" src="http://i.imgur.com/ExiL0T3h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/tHIRhpXh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/NdrRMhdh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/bbITVMlh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/AerCNanh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/G587L9xh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/ZTwnKl3h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/AcSg93ph.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/C1JOKEZh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/OOfKU1Gh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/avfcF43h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/Lq3rbZyh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/EkHUA4yh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/hIdi9dhh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/L44sXF7h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/ICk7ljJh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/hWGWuYkh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/7bkILcDh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/ixdGhD7h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/yn1RbNjh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/v2vqW7gh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/FNAYgIah.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/jWWfzEih.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/m2Zv1oKh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/kg28VY5h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/AkMZPdNh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/MZTbjZph.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/Kv4iJVlh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/VCCRdB6h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/QdPHpv7h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/IgR9RUah.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/HJWdlubh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/BMfWVm7h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/sZRUXWvh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/u2sfMTKh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/ri4ZyiXh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/wtqHr3Th.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/eWdnNdJh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/I7P8RRjh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/CcVDqmYh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/6U2HEkvh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/DUOM6KWh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/DHPJ9oTh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/qonur9mh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/beGHNQkh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/8iIPuh3h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/w2HPKA9h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/zZWSvrJh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/U716mNqh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/2gSfh6yh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/83bbCI2h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/wVf3z1Oh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/lYEx8snh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/Cfj5bESh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/WtA4vEOh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/ZrQHjHch.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/ETxkZIqh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/EE3k680h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/xu6pke8h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/MQfSeX0h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/cxNeRYXh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/zlJw5ouh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/fjx0JjEh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/IvjdYn4h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/X6Ympjrh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/5D104WEh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/RnAD0KJh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/T8CO9Exh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/5Sk4BZAh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/2GwIvEbh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/DgjkH79h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/qjt5YDih.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/ry423y4h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/D6XfanRh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/2Yb6QEFh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/6CjmRsXh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/MFZrID6h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/G1gEGbmh.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/KPSOR14h.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/QAWZG8ih.jpg" title="source: imgur.com" />
-<img class="small_200" src="http://i.imgur.com/7VSv3HNh.jpg" title="source: imgur.com" />
-
+<img class="img img200" src="http://i.imgur.com/ExiL0T3h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/tHIRhpXh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/NdrRMhdh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/bbITVMlh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/AerCNanh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/G587L9xh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/ZTwnKl3h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/AcSg93ph.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/C1JOKEZh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/OOfKU1Gh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/avfcF43h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/Lq3rbZyh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/EkHUA4yh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/hIdi9dhh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/L44sXF7h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/ICk7ljJh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/hWGWuYkh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/7bkILcDh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/ixdGhD7h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/yn1RbNjh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/v2vqW7gh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/FNAYgIah.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/jWWfzEih.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/m2Zv1oKh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/kg28VY5h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/AkMZPdNh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/MZTbjZph.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/Kv4iJVlh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/VCCRdB6h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/QdPHpv7h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/IgR9RUah.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/HJWdlubh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/BMfWVm7h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/sZRUXWvh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/u2sfMTKh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/ri4ZyiXh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/wtqHr3Th.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/eWdnNdJh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/I7P8RRjh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/CcVDqmYh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/6U2HEkvh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/DUOM6KWh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/DHPJ9oTh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/qonur9mh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/beGHNQkh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/8iIPuh3h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/w2HPKA9h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/zZWSvrJh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/U716mNqh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/2gSfh6yh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/83bbCI2h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/wVf3z1Oh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/lYEx8snh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/Cfj5bESh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/WtA4vEOh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/ZrQHjHch.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/ETxkZIqh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/EE3k680h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/xu6pke8h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/MQfSeX0h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/cxNeRYXh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/zlJw5ouh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/fjx0JjEh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/IvjdYn4h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/X6Ympjrh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/5D104WEh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/RnAD0KJh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/T8CO9Exh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/5Sk4BZAh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/2GwIvEbh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/DgjkH79h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/qjt5YDih.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/ry423y4h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/D6XfanRh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/2Yb6QEFh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/6CjmRsXh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/MFZrID6h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/G1gEGbmh.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/KPSOR14h.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/QAWZG8ih.jpg" title="source: imgur.com" />
+<img class="img img200" src="http://i.imgur.com/7VSv3HNh.jpg" title="source: imgur.com" />
+</div>
 @stop
