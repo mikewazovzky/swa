b394eb001b35615701e9c7ed2261be67aeaa416f
Mike Wazovzky <mike.wazovzky@gmail.com>
2016-12-17 23:27:43 +0300

    Content: Valley Of Fire

--- resources/views/locations/locations/default.blade.php
+++ resources/views/locations/locations/default.blade.php
@@ -1,6 +1,6 @@
 @extends('locations.location')
 @section('location')
-<h1>Title: {{ $location->title }}</h1>
+<h1>{{ $location->title }}</h1>
 <img class="" src="/media/{{ $location->image ? : 'canyon.jpg' }}" style="width: 300px; float:right; margin: 0 10px;">
 <div class="">
 {!! $contents !!}
--- resources/views/locations/locations/vofire.blade.php
+++ resources/views/locations/locations/vofire.blade.php
@@ -4,21 +4,21 @@
 <img class="img-responsive center-block" style="float: left; max-height: 200px; margin-right: 10px;" src="{{ $images['001']['src'] }}"/>
 
 <h1>Долина огня</h1>
-<p>	- парк штата Невада (Valley of Fire State Park, Nevada) расположен в пустыне в полчасе езды на машине к западу 
+<p>	- парк штата Невада (Valley of Fire State Park, Nevada) расположен в пустыне в получасе езды на машине к западу 
 	от Лас Вегаса. Летом здесь очень жарко, поэтому рекомендуется посещать парк в ранние утренние часы, с 05-06 до 
 	09-10 часов утра, пока солнце не раскалило камни и его мягкие лучи раскрашивают окрестности в удивительно 
 	яркие цвета.</p>
 
-<p> В это время (до 09:00) будка рейнджера на въезде в парк еще закрыта и необходимо самостоятельно оплатить его 
-	посещение, запихнув в специально установленный ящик конвертик с 10 долларами (за один автомобиль), корешок от 
+<p> В это время (до 09:00) будка рейнджера на въезде в парк еще закрыта, и необходимо самостоятельно оплатить его 
+	посещение, запихнув в специально установленный ящик конвертик с 10 долларами (за один автомобиль). Корешок от 
 	конверта нужно положить под лобовое стекло. Пропуск-абонемент в Национальные парки не действует, т.к. 
 	парк принадлежит штату.</p>
 	
 <p>	Поскольку время посещения парка ограничено несколькими утреними часами, имеет смысл немного подготовиться и 
 	предварительно решить, что именно вы собираетесь посмотреть. В парке много интересного, на 
 	<a href="http://www.valley-of-fire.com/">официальном сайте</a> есть схема с указанием основных троп и 
-	достопримечательностей, но есть целые сообщества изучающие все интересные образования и публикующие их 
-	каталоги, с фотографиями и точными координатами.</p>
+	достопримечательностей. Кроме того есть энтузиасты и целые сообщества, изучающие все интересные образования и 
+	публикующие их каталоги, с фотографиями и точными координатами.</p>
 
 <p> Как показал мой опыт, даже имея координыты и подробное описание, что и где искать некоторые объекты найти 
 	не просто. Из-за пересечного рельефа можно довольно долго блуждать вокруг какой0нибудь арки и не заметить ее. 
@@ -29,7 +29,7 @@
 	<img class="img-responsive center-block" src="{{ $images['000']['src'] }}"/>
 </div>
 
-<p>В качестве первой точки для знакомства с парком я выбрал Rainbow Vista Area</p>
+<p>	В качестве первой точки для знакомства с парком я выбрал зону Rainbow Vista Area. Возле дороги есть парковка.</p>
 <div class="row">
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['003']['src'] }}"></div>				
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['004']['src'] }}"></div>			
@@ -39,7 +39,7 @@
 	<img class="img-responsive center-block" src="{{ $images['002']['src'] }}">
 </div>
 
-<p>От автомобильноя стоянки можно дойти до волны. The Wave</p>
+<p>	От парковки идем по тропе к Волне (The Wave) и Розовому каньону (Pink Canyon). <br>Волна.</p>
 <div class="row">
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['006']['src'] }}"></div>				
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['007']['src'] }}"></div>			
@@ -50,8 +50,10 @@
 	<img class="img-responsive center-block" src="{{ $images['008']['src'] }}">
 </div>
 
-<p> Дальше официальной тропы нет, но можно пройти по розовому каньону (Pink Canyon). Каньон местами очень узкий, 
-    пробираясь по нему можно увидеть каменные породы всех оттенков красного-фиолетового.</p>
+<p> Дальше официальной тропы нет, но можно пройти по розовому каньону. Местами он очень узкий, 
+    пробираясь по нему можно увидеть каменные породы всех оттенков красного-фиолетового. 
+	Несмотря на небольшие расстояния видимых ориентиров кроме самого каньона нет, если отойти 
+	в сторону, можно легко заблудиться.</p>
 <div class="row">	
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['018']['src'] }}"></div>	
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['013']['src'] }}"></div>			
@@ -61,6 +63,7 @@
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['027']['src'] }}"></div>		
 </div>
 
+<p> Розовый Каньон.</p>
 <div class="image-solo">
 	<img class="img-responsive center-block" src="{{ $images['019']['src'] }}">
 </div>
@@ -71,8 +74,8 @@
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['023']['src'] }}"></div>			
 </div>
 
-<p> По каньону можно вернуться к дороге и перейдя через нее оказаться недалеко от Безумного Холма (Crazy Hill). 
-    Действительно выглядит так, как-будто кто-то расскрасил его кисточной.</p>
+<p> По каньону можно вернуться к дороге и, перейдя через нее, оказаться недалеко от Безумного Холма (Crazy Hill). 
+    Действительно выглядит так, как-будто кто-то (безумный художник) расскрасил его кисточной.</p>
 <div class="row">	
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['028']['src'] }}"></div>	
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['029']['src'] }}"></div>
@@ -111,14 +114,16 @@
 	<div class="image-item col-sm-6"><img class="img-responsive center-block tmp" src="{{ $images['056']['src'] }}"></div>			
 </div>
 
-<p>Огненный Каньон (Fire Canyon). Представляю, как загорается в лучах рассвета и заката.</p>
+<p>	Следующая остановка - Огненный Каньон (Fire Canyon). Представляю, как загорается в лучах рассвета и заката. 
+	Здесь я попал под дождь, при такой температуре никакого дискомфорта он не доставил, но фотоаппарат 
+	пришлось спрятать.</p>
 <div class="row">	
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['057']['src'] }}"></div>		
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['058']['src'] }}"></div>	
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['060']['src'] }}"></div>			
 </div>
 
-<p>Atlatl rock с древнеиндейскими рисункам.</p>
+<p>Далее едем к Atlatl rock, это огромный камень древними индейскими рисункам.</p>
 <div class="image-solo">
 	<img class="img-responsive center-block" src="{{ $images['062']['src'] }}">
 </div>
@@ -133,7 +138,7 @@
 	<img class="img-responsive center-block vertical" src="{{ $images['065']['src'] }}">
 </div>
 
-<p>Бурундуки</p>
+<p> Рядом с ним Picknik Area, возле которой пасутся бурундуки, совершенно тщедушные по сравнению с лесными собратьями.</p>
 <div class="row">	
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['067']['src'] }}"></div>		
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['068']['src'] }}"></div>	
@@ -160,7 +165,7 @@
 	<div class="image-item col-sm-4"><img class="img-responsive center-block" src="{{ $images['078']['src'] }}"></div>
 </div>
 
-<p>На выезде из парка стоит Слоню Elephan Rock.</p>
+<p>На выезде из парка стоит Слон - Elephan Rock.</p>
 <div class="row">	   
 	<div class="image-item col-sm-6"><img class="img-responsive center-block" src="{{ $images['079']['src'] }}"></div>				
 	<div class="image-item col-sm-6"><img class="img-responsive center-block" src="{{ $images['080']['src'] }}"></div>
